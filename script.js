b8f51d9704f889c58cebba22feec7ae21b0602b1
Daniel-Hppy <daniel@richcloud.net>
2026-01-05 14:11:13 +0800

    Finished the Calculator

---
+++ script.js
@@ -0,0 +1,74 @@
+const input = document.querySelector("input");
+const keys = document.querySelectorAll(".keys");
+const equalKey = document.querySelector(".equalKey");
+const allClear = document.querySelector(".allClear");
+const clear = document.querySelector(".clear")
+
+function operate() {
+    const value = input.value;
+
+    if (hasMultipleOperators(value)){
+        input.value = "ERROR";
+        return;
+    }
+
+    let operator;
+    if (value.includes('+')) operator = '+';
+    else if (value.includes('-')) operator = '-';
+    else if (value.includes('*')) operator = '*';
+    else if (value.includes('/')) operator = '/';
+    else return;
+
+    let equationPart = value.split(operator);
+
+    let num1 = +equationPart[0];
+    let num2 = +equationPart[1];
+
+    let result;
+    if (operator === '+') result = num1 + num2;
+    if (operator === '-') result = num1 - num2;
+    if (operator === '*') result = num1 * num2;
+    if (operator === '/') {
+        if (num2 === 0){
+            return "ERROR";
+        } else {
+        result = num1 / num2;
+        }
+    }
+    return result;
+}
+
+function hasMultipleOperators(value) {
+    const operators = ["+", "-", "*", "/"];
+    let count = 0;
+
+    for (let char of value) {
+        if (operators.includes(char)) {
+            count++;
+        }
+    }
+    return count > 1;
+}
+
+keys.forEach(button => {
+    button.addEventListener("click", () => {
+        input.value += button.textContent;
+    });
+});
+
+equalKey.addEventListener("click", () => {
+    const result = operate();
+    if (result !== undefined) {
+        input.value = result;
+    }
+});
+
+allClear.addEventListener("click", () => {
+    input.value = "";
+});
+
+clear.addEventListener("click", () => {
+    const display = input.value;
+    const newDisplay = display.slice(0, -1);
+    input.value = newDisplay;
+});
